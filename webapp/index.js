6be2b6389e9e7511fec3dbb64f5ed2b8ffb0972a
Avinash maity <avinashmaity2@gmail.com>
2025-02-11 09:29:10 +0000

    Structuring Applications via Components

--- webapp/index.js
+++ webapp/index.js
@@ -1,10 +1,24 @@
-sap.ui.define(["sap/ui/core/mvc/XMLView"],function(XMLView){
+sap.ui.define(["sap/ui/core/ComponentContainer"],function(ComponentContainer){
 "use strict";
 
- XMLView.create({
-  id: "First",
-  viewName: "sap.training.exc.view.First"
- }).then(function(e){
-   e.placeAt("cont");
- });
+//  XMLView.create({
+//   id: "First",
+//   viewName: "sap.training.exc.view.First"
+//  }).then(function(e){
+//    e.placeAt("cont");
+//  });
+
+var oContainer = new ComponentContainer({
+  id : "container",
+  name : "sap.training.exc",
+  manifest : true,
+  async : true,
+  settings : {
+    id: "sap.training.exc"
+  }
 });
+
+oContainer.placeAt("cont");
+
+});
+
