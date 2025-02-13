36742779a6ee33b2061ab605e0ca69c62f28cca2
Avinash maity <avinashmaity2@gmail.com>
2025-02-13 09:28:53 +0000

    Localization - Working with a Resource Model Objective

--- webapp/model/formatter.js
+++ webapp/model/formatter.js
@@ -1,12 +1,13 @@
-sap.ui.define([], function(){
+sap.ui.define(["sap/base/i18n/ResourceBundle"], function(ResourceBundle){
 "use strict";
 
 return {
     classText : function(cls){
+        var oResourceBundle = ResourceBundle.create({ url: "i18n/i18n.properties" });
         switch(cls){
-            case "C" : return "Business class";
-            case "Y" : return "Economy class";
-            case "F" : return "First class";
+            case "C" : return oResourceBundle.getText("flightClassC");
+            case "Y" : return oResourceBundle.getText("flightClassY");
+            case "F" : return oResourceBundle.getText("flightClassF");
             default : return cls;
         }
     }
